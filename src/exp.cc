e65ae620f9a70f436c6f4ddd505e3d83f5893849
risenfromashes <ashrafurkhan37@gmail.com>
2022-08-06 05:48:26 +0600

    make awaitable tasks work

--- src/exp.cc
+++ src/exp.cc
@@ -1,319 +1,216 @@
-#include <cctype>
-#include <cstdint>
-#include <cstring>
-#include <dirent.h>
-
-#include <cassert>
 #include <concepts>
-#include <functional>
+#include <coroutine>
+#include <cstdio>
 #include <iostream>
-#include <map>
-#include <memory>
 #include <optional>
-#include <unordered_map>
-#include <vector>
 
-enum class HttpMethod { GET, POST, PUT, HEAD, PATCH, DELETE, OPTIONS };
+// basic coroutine resumable task
+namespace hm {
 
-struct RouteNodeData {
-  enum class Type { ROOT, CONSTANT, PARAM_INT, PARAM_FLOAT, PARAM_STRING };
+template <class T = void> struct Promise;
+template <class T = void> struct Awaitable;
 
-  Type type;
-  std::string_view label;
-};
-
-constexpr bool operator==(const RouteNodeData &a, const RouteNodeData &b) {
-  return a.type == b.type &&
-         (a.type == RouteNodeData::Type::ROOT || a.label == b.label);
-}
-
-struct RouteNode;
-
-constexpr bool operator==(const RouteNode &a, const RouteNodeData &b);
-constexpr bool operator==(const RouteNodeData &b, const RouteNode &a);
-
-template <> struct std::hash<RouteNodeData> {
-  std::size_t operator()(const RouteNodeData &d) const noexcept {
-    auto h1 = std::hash<std::string_view>{}(d.label);
-    auto h2 = std::hash<RouteNodeData::Type>{}(d.type);
-    return h1 ^ (h2 << 1);
-  }
-};
-
-struct RouteNode {
-
-  constexpr static size_t npos = static_cast<size_t>(-1);
-
-  RouteNodeData data;
-  bool terminal;
-  uint32_t methods;
-  uint16_t route_index[8]; // there shouldn't be more than 2^16 handlers
-  std::vector<RouteNode> children;
-
-  // create empty root
-  constexpr RouteNode() {
-    data.type = RouteNodeData::Type::ROOT;
-    terminal = false;
-  }
-
-  constexpr RouteNode(const RouteNodeData &data) : data(data) {}
-
-  constexpr RouteNode(RouteNode &&b)
-      : data(b.data), terminal(b.terminal), methods(b.methods),
-        children(std::move(b.children)) {
-    std::copy(b.route_index, b.route_index + 8, route_index);
-  }
-
-  constexpr RouteNode &operator=(RouteNode &&b) {
-    data = b.data;
-    terminal = b.terminal;
-    methods = b.methods;
-    children = std::move(b.children);
-    std::copy(b.route_index, b.route_index + 8, route_index);
-    return *this;
-  }
-
-  RouteNode(const RouteNode &) = delete;
-  RouteNode &operator=(const RouteNode &) = delete;
-
-  constexpr void insert_path(HttpMethod method, std::string_view path,
-                             size_t handler_index) {
-    auto m = (uint32_t)method;
-    methods |= (1 << m);
-
-    assert(path[0] == '/');
-    auto end = path.find_first_of('/', 1);
-    auto level = path.substr(1, end - 1);
-
-    auto next_path = end == path.npos ? "/" : path.substr(end);
-
-    using enum RouteNodeData::Type;
-
-    RouteNodeData data;
-    if (!level.empty() && level.front() == '{' && level.back() == '}') {
-      auto type_beg = level.find(':');
-      auto type_end = level.length() - 1;
-
-      if (type_beg == level.npos) {
-        data.label = level.substr(1, type_end - 1);
-      } else {
-        data.label = level.substr(1, type_beg - 1);
-      }
-      data.type = PARAM_STRING;
-
-      if (type_beg != level.npos) {
-        auto type = level.substr(type_beg + 1, type_end - type_beg - 1);
-        if (type == "int") {
-          data.type = PARAM_INT;
-        } else if (type == "float") {
-          data.type = PARAM_FLOAT;
-        }
-      }
-    } else {
-      data.label = level;
-      data.type = CONSTANT;
-    }
-
-    bool terminal = next_path == "/";
-
-    auto itr = std::find(children.begin(), children.end(), data);
-    if (itr == children.end()) {
-      auto &child = children.emplace_back(data);
-
-      child.terminal = terminal;
-
-      if (!terminal) {
-        child.insert_path(method, next_path, handler_index);
-      } else {
-        child.methods = 1 << m;
-        child.route_index[m] = handler_index;
-      }
-    } else {
-      auto &child = *itr;
-      if (!terminal) {
-        child.insert_path(method, next_path, handler_index);
-      } else {
-        child.terminal = true;
-        child.methods |= (1 << m);
-        child.route_index[m] = handler_index;
-      }
-    }
-  }
-
-  /* returns handler index */
-  constexpr std::size_t match(HttpMethod method, std::string_view path,
-                              std::vector<std::string_view> &vars) {
-
-    auto m = (uint32_t)method;
-
-    if ((methods & (1 << m)) == 0) {
-      return npos;
-    }
-
-    assert(path[0] == '/');
-    auto end = path.find_first_of('/', 1);
-    auto level = path.substr(1, end - 1);
-
-    auto next_path = end == path.npos ? "/" : path.substr(end);
-
-    // std::cout << "path: " << path << std::endl;
-    // std::cout << "next path: " << next_path << std::endl;
-    // std::cout << "label: " << data.label << std::endl;
-
-    using enum RouteNodeData::Type;
-
-    switch (data.type) {
-    case ROOT: // matches everything
-      next_path = path;
-      break;
-    case CONSTANT: // only matches if level string matches
-      if (data.label != level) {
-        return npos;
-      }
-      break;
-    case PARAM_INT: // check if its really an int
-      for (auto &ch : level) {
-        if (!std::isdigit(ch)) {
-          return npos;
-        }
-      }
-      vars.emplace_back(level);
-      break;
-    case PARAM_FLOAT: { // requirements of number + atmost one dot
-      int n_dot = 0;
-      for (auto &ch : level) {
-        if (!std::isdigit(ch) && ch != '.') {
-          return npos;
-        } else if (ch == '.') {
-          if (n_dot++) { // just one decimal point
-            return npos;
-          }
-        }
-      }
-      vars.emplace_back(level);
-    } break;
-    case PARAM_STRING:
-      // nothing to match here, anything passes
-      vars.emplace_back(level);
-      break;
-    }
-
-    for (auto &child : children) {
-      if (auto r = child.match(method, next_path, vars); r != npos) {
-        return r;
-      }
-    }
-
-    if (terminal) {
-      return route_index[m];
-    }
-
-    switch (data.type) {
-    case PARAM_FLOAT:
-    case PARAM_INT:
-    case PARAM_STRING:
-      vars.pop_back();
-      break;
-    default:
-      break;
-    }
-
-    return npos;
-  }
-};
-
-constexpr bool operator==(const RouteNode &a, const RouteNodeData &b) {
-  return a.data == b;
-}
-
-constexpr bool operator==(const RouteNodeData &b, const RouteNode &a) {
-  return a == b;
-}
-
-class Router {
+template <class T = void> class Task {
 public:
-  struct HandlerData {
-    Router *router;
-    size_t handler_index;
+  using promise_type = Promise<T>;
 
-    constexpr std::optional<std::string_view>
-    get_param(std::string_view label) const {
-      auto route = std::string_view(router->route_paths_[handler_index]);
-      // goto bracket, match label, repeat
-      size_t pos, index = 0;
-      while ((pos = route.find('{')) != route.npos) {
-        if (route.substr(pos + 1, label.size()) == label) {
-          std::cout << "index: " << index << std::endl;
-          return router->vars_[index];
-        }
-        route = route.substr(pos + 1);
-        index++;
-      }
-      return std::nullopt;
-    }
-  };
+  using handle_type = std::coroutine_handle<promise_type>;
 
-public:
-  void add_route(HttpMethod method, const char *route_path,
-                 std::invocable<HandlerData> auto &&handler) {
-    assert(handlers_.size() == route_paths_.size());
-    size_t handler_index = handlers_.size();
-    handlers_.push_back(std::move(handler));
-    route_paths_.push_back(std::move(route_path));
-    root_.insert_path(method, route_path, handler_index);
+  Task(handle_type handle) : handle_(handle) {}
+  Task(Task &&other) : handle_(other.handle_) { other.handle_ = nullptr; }
+  Task &operator=(Task &&other) {
+    handle_.destroy();
+    handle_ = other.handle_;
+    other.handle_ = nullptr;
   }
 
-  void dispatch_route(HttpMethod method, std::string_view path) {
-    vars_.clear();
-    auto index = root_.match(method, path, vars_);
-    if (index != RouteNode::npos) {
-      HandlerData data{.router = this, .handler_index = index};
-      std::invoke(handlers_[index], data);
-    } else {
-      std::cerr << "Path: " << path << " didn't match any route" << std::endl;
+  Task(const Task &) = delete;
+  Task &operator=(const Task &) = delete;
+
+  void resume() {
+    if (handle_ && !handle_.done()) {
+      handle_.resume();
     }
   }
 
-private:
-  RouteNode root_;
-  std::vector<std::string_view> vars_;
-  std::vector<std::function<void(const HandlerData &)>> handlers_;
-  std::vector<const char *> route_paths_;
+  void
+  set_result(std::convertible_to<T> auto &&v) requires(!std::same_as<T, void>) {
+    if (handle_ && !handle_.done()) {
+      handle_.promise().set_value(std::forward<decltype(v)>(v));
+    }
+  }
+
+  std::optional<T> get_result() requires(!std::same_as<T, void>) {
+    if (handle_ && handle_.done()) {
+      return handle_.promise().value_;
+    }
+    return std::nullopt;
+  }
+
+  ~Task() {
+    // if (handle_) {
+    //   handle_.destroy();
+    // }
+  }
+
+  handle_type handle_;
 };
+
+template <class T> struct Promise {
+  using handle_type = std::coroutine_handle<Promise<T>>;
+
+  T value_;
+  std::coroutine_handle<> continuation_ = nullptr;
+
+  auto initial_suspend() noexcept {
+    std::cout << "initial suspend Promise<T>" << std::endl;
+    // not lazy start immediately
+    return std::suspend_never{};
+  }
+
+  auto final_suspend() noexcept {
+    std::cout << "final suspend Promise<T>" << std::endl;
+    // always suspend let destructor of task destroy coroutine
+    struct awaitable {
+      using promise_type = Promise<T>;
+      using handle_type = std::coroutine_handle<Promise<T>>;
+
+      std::coroutine_handle<> continuation_;
+
+      bool await_ready() noexcept { return false; }
+
+      auto await_suspend(std::coroutine_handle<> handle) noexcept {
+        return continuation_;
+      }
+
+      void await_resume() noexcept {}
+
+      awaitable(std::coroutine_handle<> cont) : continuation_(cont) {}
+    } aw(continuation_);
+    return aw;
+  }
+
+  void unhandled_exception() {}
+
+  Task<T> get_return_object() {
+    // return task to caller
+    return handle_type::from_promise(*this);
+  }
+
+  Task<T> get_task() { return get_return_object(); }
+
+  void set_value(std::convertible_to<T> auto &&v) {
+    value_ = std::forward<decltype(v)>(v);
+  }
+
+  void return_value(std::convertible_to<T> auto &&v) {
+    std::cout << "return_value: " << v << std::endl;
+    set_value(std::forward<decltype(v)>(v));
+  }
+};
+
+template <> struct Promise<void> {
+  using handle_type = std::coroutine_handle<Promise<>>;
+
+  std::coroutine_handle<> continuation_ = nullptr;
+
+  auto initial_suspend() noexcept {
+    std::cout << "initial suspend Promise<>" << std::endl;
+    // not lazy start immediately
+    return std::suspend_never{};
+  }
+
+  auto final_suspend() noexcept {
+    std::cout << "final suspend Promise<>" << std::endl;
+    // always suspend let destructor of task destroy coroutine
+    return std::suspend_never{};
+  }
+
+  void unhandled_exception() {}
+
+  Task<> get_return_object() {
+    // return task to caller
+    return handle_type::from_promise(*this);
+  }
+
+  Task<> get_task() { return get_return_object(); }
+
+  void return_void() {
+    if (continuation_) {
+      continuation_.resume();
+    }
+  }
+};
+
+template <class T> auto operator co_await(Task<T> &&t) {
+  std::cout << "co_await Task<T>" << std::endl;
+  struct awaitable {
+    using promise_type = Promise<T>;
+    using handle_type = std::coroutine_handle<Promise<T>>;
+    std::coroutine_handle<> handle_;
+    bool await_ready() {
+      std::cout << "await_ready" << std::endl;
+      return false;
+    }
+    void await_suspend(std::coroutine_handle<Promise<>> handle) {
+      std::cout << "f() suspend" << std::endl;
+      std::cout << "handle: " << handle.address() << " done: " << handle.done()
+                << std::endl;
+      std::cout << "task handle: " << task_.handle_.address()
+                << " done: " << task_.handle_.done() << std::endl;
+      task_.handle_.promise().continuation_ = handle;
+      // task_.handle_.resume();
+    }
+    T await_resume() {
+      std::cout << "Task<> resumed" << std::endl;
+      return task_.get_result().value();
+    }
+    Task<T> task_;
+    awaitable(Task<T> &&t) : task_(std::move(t)) {}
+  } aw(std::move(t));
+  return aw;
+}
+
+auto this_coro() {
+  struct {
+    using handle_type = std::coroutine_handle<>;
+    handle_type handle_;
+    bool await_ready() { return false; }
+    void await_suspend(handle_type handle) {
+      std::cout << "this coro" << std::endl;
+      handle_ = handle;
+      handle.resume();
+    }
+    handle_type await_resume() { return handle_; }
+  } aw;
+  return aw;
+}
+
+} // namespace hm
+
+using namespace hm;
+std::coroutine_handle<> coro;
+// inner task
+Task<int> f1() {
+  std::cout << "entering f1()" << std::endl;
+  coro = co_await this_coro();
+  std::cout << "suspending coro" << std::endl;
+  co_await std::suspend_always{};
+  std::cout << "f1() resumed" << std::endl;
+  co_return 69;
+}
+// outer task
+Task<> f() {
+  std::cout << "entering f()" << std::endl;
+  // co_await f1();
+  int x = co_await f1();
+  std::cout << "resumed f() " << std::endl;
+  std::cout << "fuck" << std::endl;
+  std::cout << x << std::endl;
+}
 
 int main() {
-  Router handler;
-  handler.add_route(HttpMethod::GET, "/",
-                    [](auto) { std::cout << "GET /" << std::endl; });
-  handler.add_route(HttpMethod::GET, "/api",
-                    [](auto) { std::cout << "GET /api" << std::endl; });
-  handler.add_route(HttpMethod::POST, "/api",
-                    [](auto) { std::cout << "POST /api" << std::endl; });
-
-  handler.add_route(HttpMethod::GET, "/api/{id:int}/messages",
-                    [](const Router::HandlerData &data) {
-                      std::cout << "GET /api/{id}/messages" << std::endl;
-                      auto id = data.get_param("id");
-                      assert(id.has_value());
-                      std::cout << "id: " << id.value() << std::endl;
-                    });
-
-  handler.add_route(HttpMethod::POST, "/api/{id:int}/messages/{to:int}/{text}",
-                    [](const Router::HandlerData &data) {
-                      std::cout << "POST /api/{id}/messages/{to}/{text}"
-                                << std::endl;
-                      auto id = data.get_param("id");
-                      auto to = data.get_param("to");
-                      auto text = data.get_param("text");
-                      assert(id.has_value() && to.has_value());
-                      std::cout << "id: " << id.value() << std::endl;
-                      std::cout << "to: " << to.value() << std::endl;
-                      std::cout << "text: " << text.value() << std::endl;
-                    });
-
-  handler.dispatch_route(HttpMethod::GET, "/");
-  handler.dispatch_route(HttpMethod::GET, "/api");
-  handler.dispatch_route(HttpMethod::POST, "/api");
-  handler.dispatch_route(HttpMethod::GET, "/api/69/messages");
-  handler.dispatch_route(HttpMethod::POST, "/api/69/messages/77/blahblah");
+  auto t = f();
+  std::cout << "in main" << std::endl;
+  coro.resume();
 }
